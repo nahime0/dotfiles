8b0c04c5b91d911078d5af51f0e24fb2aa865965
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-03-05 13:21:11 +0100

    Adds setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source ./functions.sh
+
+parse_env_file .env
+
+# Backup configuration files
+if [ -f "$HOME/.ssh/config" ] && [ ! -L "$HOME/.ssh/config" ]; then
+    log_string "Backing up ssh config file"
+    mv "$HOME/.ssh/config" "$HOME/.ssh/config.bak"
+fi
+
+# Symlinking configuration files from this repo
+if [ ! -L "$HOME/.ssh/config" ]; then
+    if [ -f "$ALTERNATIVE_SSH_KEY_FILE" ]; then
+        log_string "Symlinking alternative ssh config file"
+        ln -s "$(pwd)/ssh/config.alternative.conf" "$HOME/.ssh/config"
+    else
+        log_string "Symlinking regular ssh config file"
+        ln -s "$(pwd)/ssh/config.conf" "$HOME/.ssh/config"
+    fi
+else
+    log_string "Symbolink link for SSH file already exists"
+fi
