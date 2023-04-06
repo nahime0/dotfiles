e8ca5de85b36f8fc50a7576d94c7661884e3b1b0
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-04-06 12:22:10 +0200

    Fixed SSH. Added git config

--- setup.sh
+++ setup.sh
@@ -6,19 +6,26 @@ parse_env_file .env
 
 # Backup configuration files
 if [ -f "$HOME/.ssh/config" ] && [ ! -L "$HOME/.ssh/config" ]; then
-    log_string "Backing up ssh config file"
-    mv "$HOME/.ssh/config" "$HOME/.ssh/config.bak"
+  log_string "Backing up ssh config file"
+  mv "$HOME/.ssh/config" "$HOME/.ssh/config.bak"
 fi
 
 # Symlinking configuration files from this repo
+
+# SSH
 if [ ! -L "$HOME/.ssh/config" ]; then
-    if [ -f "$HOME/$ALTERNATIVE_SSH_KEY_FILE" ]; then
-        log_string "Symlinking alternative ssh config file"
-        ln -s "$(pwd)/ssh/config.alternative.conf" "$HOME/.ssh/config"
-    else
-        log_string "Symlinking regular ssh config file"
-        ln -s "$(pwd)/ssh/config.conf" "$HOME/.ssh/config"
-    fi
+  log_string "Symlinking ssh config file"
+  ln -s "$(pwd)/ssh/config.conf" "$HOME/.ssh/config"
 else
-    log_string "Symbolink link for SSH file already exists"
+  log_string "Symbolink link for SSH file already exists"
+fi
+
+# Git
+if [ ! -L "$HOME/.gitconfig" ]; then
+  log_string "Symlinking git config files"
+  ln -s "$(pwd)/git/gitconfig" "$HOME/.gitconfig"
+  ln -s "$(pwd)/git/gitconfig-athomos" "$HOME/.gitconfig-athomos"
+  ln -s "$(pwd)/git/gitconfig-vuppi" "$HOME/.gitconfig-vuppi"
+else
+  log_string "Symbolink link for git file already exists"
 fi
