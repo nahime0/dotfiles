95ee4d0d52b7e1c8f372fd116cc7395c222f2480
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-11-21 17:13:01 +0100

    Use lazy instead of packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,21 @@
 -- https://github.com/nanotee/nvim-lua-guide
 
 -- Plugins
-require('user/plugins')
+-- require('user/plugins')
 
 -- User settings
 require('user/options')
 require('user/keys')
 
+-- Lazy
+require('user/lazy')
+
+
+-- Force snippets filetype for .snippets files
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.snippets",
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+      vim.api.nvim_buf_set_option(buf, "filetype", "snippets")
+    end,
+})
