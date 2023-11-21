3b5670cd10cd53ca9a0e62c87cc70b7ca7b779b2
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-11-21 22:45:08 +0100

    Removed extra spaces

--- nvim/lua/user/lazy/dashboard.lua
+++ nvim/lua/user/lazy/dashboard.lua
@@ -18,15 +18,20 @@ return {
         '         ░       ░  ░ ░  ░  ░ ░         ░      ░  ░',
         '',
         '',
+        '     https://vuppi.dev',
+        '',
+        '',
 
       },
       center = {
         { icon = '  ', desc = 'New file', action = 'enew' },
-        { icon = '  ', desc = 'Find file               ', key = 'Space + f', action = 'Telescope find_files' },
-        { icon = '  ', desc = 'Recent files            ', key = 'Space + h', action = 'Telescope oldfiles' },
-        { icon = '  ', desc = 'Find Word               ', key = 'Space + g', action = 'Telescope live_grep' },
+        { icon = '  ', desc = 'Find file', key = 'Space + f', action = 'Telescope find_files' },
+        { icon = '  ', desc = 'Recent files', key = 'Space + h', action = 'Telescope oldfiles' },
+        { icon = '  ', desc = 'Find Word', key = 'Space + g', action = 'Telescope live_grep' },
       },
-      footer = { '' }
+      footer = {
+        '',
+      }
     },
     hide = {
       statusline = false,
