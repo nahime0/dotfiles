6b6dc2be7bbe5207c583cac04ce2fe25c61f8a17
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-11-21 22:28:35 +0100

    Suppress navic errors

--- nvim/lua/user/lazy/barbecue.lua
+++ nvim/lua/user/lazy/barbecue.lua
@@ -1,12 +1,19 @@
 return {
-  "utilyre/barbecue.nvim",
+  'utilyre/barbecue.nvim',
   name = "barbecue",
+  event = 'BufRead',
   version = "*",
   dependencies = {
-    "SmiteshP/nvim-navic",
+    {
+      "SmiteshP/nvim-navic",
+      init = function()
+        -- Suppress navic annoying navic errors (cannot attach to intelephense)
+         vim.g.navic_silence = true
+      end,
+    },
     "nvim-tree/nvim-web-devicons", -- optional dependency
   },
   opts = {
-    -- configurations go here
+    theme = 'tokyonight',
   },
 }
