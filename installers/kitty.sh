3edd0e3c06af9556168bf3683a8f7e9e7595bbc6
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-11-30 12:00:50 +0100

    Removed kitty setup, integrated into the installer

--- installers/kitty.sh
+++ installers/kitty.sh
@@ -10,7 +10,11 @@ if [ ! -d /Applications/kitty.app ]; then
         $GUM style --padding "0 2" "Re run this script after the installation is complete."
         $GUM style --padding "0 2" "Press a key to continue..."
         read -n 1
-        exec $DIR/setup-kitty
+
+        # Launch the default kitty installer - updater (https://sw.kovidgoyal.net/kitty/binary/)
+        # Use this script to install or update Kitty terminal emulator
+        curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
         exit 1
     fi
 fi
