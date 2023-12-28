a62538d3df49f6021573710a4f0885168276e445
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2023-12-28 13:25:43 +0100

    chore: change gum render for pkgx force download

--- installers/pkgx-force-download.sh
+++ installers/pkgx-force-download.sh
@@ -7,38 +7,18 @@
 
 if $GUM confirm "Do you want to force the download of pkgx binaries?" ; then
     $GUM style --padding "0 2" "Downloading pkgx binaries, and printing version..."
-    {
-        $GUM style --foreground 212 "The installed version of Git is:";
-        ~/bin/git --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 
-    {
-        $GUM style --foreground 212 "The installed version of PHP is:";
-        ~/bin/php --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+    echo "git: $(~/bin/git --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 
-    {
-        $GUM style --foreground 212 "The installed version of Composer is:";
-        ~/bin/composer --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+    echo "php: $(~/bin/php --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 
-    {
-        $GUM style --foreground 212 "The installed version of Ripgrep is:";
-        ~/bin/rg --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+    echo "composer: $(~/bin/composer --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 
-    {
-        $GUM style --foreground 212 "The installed version of fzf is:";
-        ~/bin/fzf --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+    echo "ripgrep: $(~/bin/rg --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 
-    {
-        $GUM style --foreground 212 "The installed version of node is:";
-        ~/bin/node --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+    echo "fzf: $(~/bin/fzf --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 
-    {
-        $GUM style --foreground 212 "The installed version of npm is:";
-        ~/bin/npm --version
-    } | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+    echo "node: $(~/bin/node --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
+
+    echo "npm: $(~/bin/npm --version)" | $GUM style --border normal --padding "0 4" --border-foreground 212 --width 60
 fi
