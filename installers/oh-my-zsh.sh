18005f283e58532b78d453f94159339a40a1fb1e
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2024-02-13 22:41:05 +0100

    feat: do not ask for confirmation for oh-my-zsh

--- installers/oh-my-zsh.sh
+++ installers/oh-my-zsh.sh
@@ -5,21 +5,17 @@
 #######################################
 
 if [ ! -d $HOME/.oh-my-zsh ]; then
-    if $GUM confirm "Do you want to install oh-my-zsh?" ; then
-        $GUM style --padding "0 2" "Installing oh-my-zsh..."
-        $GUM style --padding "0 2" "Re run this script after the installation is complete."
-        $GUM style --padding "0 2" "Press a key to continue..."
-        read -n 1
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        exit 1
-    fi
+    $GUM style --padding "0 2" "Installing oh-my-zsh..."
+    $GUM style --padding "0 2" "Re run this script after the installation is complete."
+    $GUM style --padding "0 2" "Press a key to continue..."
+    read -k 1
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    exit 1
 fi
 
 ###############################
 # Install powerlevel10k theme #
 ###############################
 if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]; then
-    if $GUM confirm "Do you want to install powerlevel10k theme?" ; then
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-    fi
+    $DIR/bin/pkgx/git-scm/git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 fi
