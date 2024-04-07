1ebb9dcf84476026178af4d4e1a6ed9b5c76e662
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2024-04-07 22:29:07 +0200

    feat: herd config

--- config/nvim/lua/user/lazy/nvim-lspconfig.lua
+++ config/nvim/lua/user/lazy/nvim-lspconfig.lua
@@ -96,6 +96,7 @@ return {
     -- userLanguaes are custom languages for tailwind. To the nvim buffer type is associated
     -- one of the tailwindcss lsp supported languages.
     -- See here: https://github.com/tailwindlabs/tailwindcss-intellisense/blob/master/packages/tailwindcss-language-service/src/util/languages.ts
+
     require('lspconfig').tailwindcss.setup({
       capabilities = capabilities,
       init_options = {
