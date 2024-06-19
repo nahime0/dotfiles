e3d240694a0cee036cd8aec2796891dfba9f58b8
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2024-06-19 20:00:16 +0200

    feat: move to system-wide brew

--- installers/macos/homebrew.sh
+++ installers/macos/homebrew.sh
@@ -8,7 +8,9 @@ if [ ! -d $HOME/.brew ]; then
     if $GUM confirm "Do you want to install Homebrew?" ; then
 
         $GUM style --padding "0 2" "Installing Homebrew..."
-        mkdir $HOME/.brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.brew
+        # mkdir $HOME/.brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.brew
+
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
         exit 1
     fi
