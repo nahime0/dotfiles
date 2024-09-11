add7d8d659450a7c2ea5931ec563997d9839bebf
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2024-09-11 19:19:18 +0200

    feat: check if tmux directory already exists before cloning tpm

--- installers/06-clone-tmux-tpm.sh
+++ installers/06-clone-tmux-tpm.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Check if .tmux folder does not exists, in case clone the tmux plugin manager
+if [ ! -d $HOME/.tmux ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi;
