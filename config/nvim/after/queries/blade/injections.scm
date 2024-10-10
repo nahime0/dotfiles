0cea4d2919e2c13f0ccae9083a4287f81e95cdad
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2024-10-10 19:24:02 +0200

    feat: update injections for tree-sitter-blade

--- config/nvim/after/queries/blade/injections.scm
+++ config/nvim/after/queries/blade/injections.scm
@@ -2,3 +2,16 @@
     (#not-has-ancestor? @injection.content "envoy")
     (#set! injection.combined)
     (#set! injection.language php))
+
+; could be bash or zsh
+; or whatever tree-sitter grammar you have.
+((text) @injection.content
+    (#has-ancestor? @injection.content "envoy")
+    (#set! injection.combined)
+    (#set! injection.language bash))
+
+; 🚧  Available for experimental split_parser see issue #5 
+;((php_only) @injection.content
+;    (#set! injection.language php_only))
+;((parameter) @injection.content
+;    (#set! injection.language php_only))
