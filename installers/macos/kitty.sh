709e374b0f5ebf3aebfb4124543f0b60d7ff16b1
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2024-10-10 19:46:58 +0200

    remove gum from kitty setup script

--- installers/macos/kitty.sh
+++ installers/macos/kitty.sh
@@ -5,10 +5,7 @@
 #########
 
 if [ ! -d /Applications/kitty.app ]; then
-    $GUM style --padding "0 2" "Installing Kitty..."
-    $GUM style --padding "0 2" "Re run this script after the installation is complete."
-    $GUM style --padding "0 2" "Press a key to continue..."
-    read -n 1
+    echo "Kitty not found. Installing..."
 
     # Launch the default kitty installer - updater (https://sw.kovidgoyal.net/kitty/binary/)
     # Use this script to install or update Kitty terminal emulator
