60e88330a365c4f5389660269924b55c8312dd3c
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2025-01-14 18:42:08 +0100

    fix: update directory path for tpm cloning

--- installers/05-clone-tmux-tpm.sh
+++ installers/05-clone-tmux-tpm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 # Check if .tmux folder does not exists, in case clone the tmux plugin manager
-if [ ! -d $HOME/.tmux ]; then
+if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+    mkdir -p $HOME/.tmux/plugins
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi;
