953217dfe275b5ebbb6ad1880d9729ae90f98951
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2025-05-29 15:15:21 +0200

    feat: replace deprecated symbol

--- config/nvim/lua/user/lazy/lualine.lua
+++ config/nvim/lua/user/lazy/lualine.lua
@@ -29,7 +29,7 @@ return {
           symbols = { added = ' ', modified = ' ', removed = ' ' },
         },
         function ()
-          return '󰅭 ' .. vim.pesc(tostring(#vim.tbl_keys(vim.lsp.buf_get_clients())) or '')
+          return '󰅭 ' .. vim.pesc(tostring(#vim.tbl_keys(vim.lsp.get_clients())) or '')
         end,
         { 'diagnostics', sources = { 'nvim_diagnostic' } },
       },
