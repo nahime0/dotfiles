ca0c2687f36e215892e671f75bb9294249b0433f
Vincenzo Petrucci <vincenzo.petrucci@gmail.com>
2026-08-08 16:20:38 +0200

    feat: add zellij close session and zellij notes

--- installers/04-symlink-binaries.sh
+++ installers/04-symlink-binaries.sh
@@ -16,6 +16,10 @@ rm -rf $HOME/bin/zj
 ln -s $DIR/bin/zellij-switcher $HOME/bin/zj
 log_message "Created symbolic link for zellij-switcher as zj..."
 
+rm -rf $HOME/bin/zellij-close-session
+ln -s $DIR/bin/zellij-close-session $HOME/bin/zellij-close-session
+log_message "Created symbolic link for zellij-close-session..."
+
 rm -rf $HOME/bin/export-extensions
 ln -s $DIR/bin/export-extensions $HOME/bin/export-extensions
 log_message "Created symbolic link for export-extensions..."
